87869c01350aba6860fe9a8f966396bc98c5a024
Omar Tarek <omartarek711@gmail.com>
2022-11-26 00:14:01 +0200

    Changes to push to repo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD).replace('<USERNAME>', process.env.DATABASE_USERNAME);
 
 mongoose.connect(DB, {
   useNewUrlParser: true,
